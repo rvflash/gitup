08190cc66b3a4d6237a4c73d3146b50477c4034b
hgouchet <herve.gouchet@twenga.com>
2016-07-28 22:38:44 +0200

    Improve package. Add unit and functional tests

--- internal/gitflow/gitflow.go
+++ internal/gitflow/gitflow.go
@@ -12,8 +12,52 @@ const (
 )
 
 type Repo struct {
-	Path  string
+	path  string
 	valid bool
+}
+
+// Enable testing by mocking
+var execCommand = exec.Command
+
+// NewRepo starts a new Git repository.
+func NewRepo(path string) (*Repo, error) {
+	if path = strings.TrimSpace(path); path == "" {
+		return nil, errors.New(errMsgUndefinedPath)
+	}
+	r := &Repo{path: path}
+	if err := r.gitCheck(); err != nil {
+		return nil, err
+	}
+	return r, nil
+}
+
+// LocalTag returns the most recent tag reachable from the defined repository.
+func (r *Repo) LocalTag() (string, error) {
+	return r.gitDescribe("")
+}
+
+// LastTag returns the last available tag of the Git repository.
+func (r *Repo) LastTag() (tag string, err error) {
+	// Get new tags from the remote
+	if err = r.gitFetch(); err == nil {
+		// Get the latest commit of tag list
+		var commit []byte
+		if commit, err = execCommand("git", "-C", r.path, "rev-list", "--tags", "--max-count=1").Output(); err == nil {
+			// Get the tag name for this commit
+			tag, err = r.gitDescribe(string(commit))
+		}
+	}
+	return
+}
+
+// CheckoutTag returns an error if it can not switch the repository on this tag
+func (r *Repo) CheckoutTag(tag string) (err error) {
+	if err = r.gitCheck(); err != nil {
+		return
+	} else if tag = strings.TrimSpace(tag); tag == "" {
+		return errors.New(errMsgUndefinedTag)
+	}
+	return execCommand("git", "-C", r.path, "checkout", tag).Run()
 }
 
 // gitCheck returns err if path is not a valid Git repository.
@@ -29,12 +73,12 @@ func (r *Repo) gitDescribe(commit string) (tag string, err error) {
 	if err = r.gitCheck(); err != nil {
 		return
 	}
-	args := []string{"-C", r.Path, "describe", "--abbrev=0", "--tags"}
+	args := []string{"-C", r.path, "describe", "--abbrev=0", "--tags"}
 	if commit = strings.TrimSpace(commit); commit != "" {
 		args = append(args, commit)
 	}
 	var ref []byte
-	if ref, err = exec.Command("git", args...).Output(); err == nil {
+	if ref, err = execCommand("git", args...).Output(); err == nil {
 		tag = strings.TrimSpace(string(ref))
 	}
 	return
@@ -45,52 +89,10 @@ func (r *Repo) gitFetch() (err error) {
 	if err = r.gitCheck(); err != nil {
 		return
 	}
-	return exec.Command("git", "-C", r.Path, "fetch", "--tags").Run()
+	return execCommand("git", "-C", r.path, "fetch", "--tags").Run()
 }
 
 // gitStatus returns the working tree status.
-func (r *Repo) gitStatus() (status []byte, err error) {
-	if r.Path = strings.TrimSpace(r.Path); r.Path == "" {
-		err = errors.New(errMsgUndefinedPath)
-	} else {
-		status, err = exec.Command("git", "-C", r.Path, "status").Output()
-	}
-	return
-}
-
-// LocalTag returns the most recent tag reachable from the defined repository.
-func (r *Repo) LocalTag() (string, error) {
-	return r.gitDescribe("")
-}
-
-// LastTag returns the last available tag of the Git repository.
-func (r *Repo) LastTag() (tag string, err error) {
-	// Get new tags from the remote
-	if err = r.gitFetch(); err == nil {
-		// Get the latest commit of tag list
-		var commit []byte
-		if commit, err = exec.Command("git", "-C", r.Path, "rev-list", "--tags", "--max-count=1").Output(); err == nil {
-			// Get the tag name for this commit
-			tag, err = r.gitDescribe(string(commit))
-		}
-	}
-	return
-}
-
-// CheckoutTag returns an error if it can not switch the repository on this tag
-func (r *Repo) CheckoutTag(tag string) (err error) {
-	if err = r.gitCheck(); err != nil {
-		return
-	} else if tag = strings.TrimSpace(tag); tag == "" {
-		return errors.New(errMsgUndefinedTag)
-	}
-	return exec.Command("git", "-C", r.Path, "checkout", tag).Run()
-}
-
-// IsRepository return true if the repo is a valid Git repository.
-func (r *Repo) IsRepository() bool {
-	if err := r.gitCheck(); err != nil {
-		return r.valid
-	}
-	return false
+func (r *Repo) gitStatus() ([]byte, error) {
+	return execCommand("git", "-C", r.path, "status").Output()
 }
